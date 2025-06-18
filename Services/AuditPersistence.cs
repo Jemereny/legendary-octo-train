36e61e5ef53cb139060913856d920fe1cf23cef2
Jeremy Chan <chanjem@gmail.com>
2025-06-18 22:03:01 +0800

    Change list to concurrent bag

--- Services/AuditPersistence.cs
+++ Services/AuditPersistence.cs
@@ -2,14 +2,13 @@
 using SingularHealth.Models.Persistence;
 using SingularHealth.Models.Domain;
 using SingularHealth.Mappers;
+using System.Collections.Concurrent;
 
 namespace SingularHealth.Services;
 
 public class AuditPersistence
 {
-  // Currently not thread safe - probably need create a concurrent list using locking
-  // Should work for now
-  private List<PersistentAudit> audits = new();
+  private ConcurrentBag<PersistentAudit> audits = new();
 
   public IEnumerable<Audit> GetAudits()
   {
